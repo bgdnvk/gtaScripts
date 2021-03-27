af96e8cba6ec8680816c117d51b594f8d6a3b4c5
bgdnvk <bnovykov@gmail.com>
2021-03-27 22:19:16 +0100

    vehiclespawner

---
+++ testbog/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'bog'
+description 'desc'
+version '0.0.1'
+
+--resource_type 'gametype' { name = 'My awesome game type!' }
+
+client_script 'testbog_client.js'
