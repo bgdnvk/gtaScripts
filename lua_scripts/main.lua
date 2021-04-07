e01466eedfc7a77a91fa75cfc9e87c01a6bf00bd
bgdnvk <bnovykov@gmail.com>
2021-04-07 02:37:36 +0200

    lua debug

---
+++ lua_scripts/main.lua
@@ -0,0 +1,2 @@
+-- https://stackoverflow.com/questions/60193174/how-to-run-lua-script-from-within-vs-code
+print('hi')
--- testbog/client.js
+++ testbog/client.js
@@ -1,47 +1,78 @@
-Delay = (ms) => new Promise(res => setTimeout(res, ms));
+// Delay = (ms) => new Promise(res => setTimeout(res, ms));
 
-RegisterCommand('car', async (source, args, raw) => {
-  // account for the argument not being passed
-  let model = "adder";
-  if (args.length > 0)
-  {
-    model = args[0].toString();
-  }
+// RegisterCommand('bogcar', async (source, args, raw) => {
+//   // account for the argument not being passed
+//   let model = "adder";
+//   if (args.length > 0)
+//   {
+//     model = args[0].toString();
+//   }
 
-  // check if the model actually exists
-  const hash = GetHashKey(model);
-  if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
-  {
+//   // check if the model actually exists
+//   const hash = GetHashKey(model);
+//   if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
+//   {
+//     emit('chat:addMessage', {
+//       args: [`It might have been a good thing that you tried to spawn a ${model}. Who even wants their spawning to actually ^*succeed?`]
+//     });
+//     return;   
+//   }
+
+//   // Request the model and wait until the game has loaded it
+//   RequestModel(hash);
+//   while (!HasModelLoaded(hash))
+//   {
+//     await Delay(500);
+//   }
+
+//   const ped = PlayerPedId();
+
+//   // Get the coordinates of the player's Ped (their character)
+//   const coords = GetEntityCoords(ped);
+
+//   // Create a vehicle at the player's position
+//   const vehicle = CreateVehicle(hash, coords[0], coords[1], coords[2], GetEntityHeading(ped), true, false);
+
+//   // Set the player into the drivers seat of the vehicle
+//   SetPedIntoVehicle(ped, vehicle, -1);
+
+//   // Allow the game engine to clean up the vehicle and model if needed
+//   SetEntityAsNoLongerNeeded(vehicle);
+//   SetModelAsNoLongerNeeded(model);
+
+//   // Tell the player the car spawned
+//   emit('chat:addMessage', {
+//     args: [`Woohoo! Enjoy your new ^*${model}!`]
+//   });
+// }, false);
+
+// const spawnPos = [686.245, 577.950, 130.461];
+
+// on('onClientGameTypeStart', () => {
+
+//   exports.spawnmanager.setAutoSpawnCallback(() => {
+//     exports.spawnmanager.spawnPlayer({
+//       x: spawnPos[0],
+//       y: spawnPos[1],
+//       z: spawnPos[2],
+//       model: 'a_m_m_skater_01'
+//     }, () => {
+//       emit('chat:addMessage', {
+//         args: [
+//           'Welcome to the party!~'
+//         ]
+//       })
+//     });
+//   });
+
+//   exports.spawnmanager.setAutoSpawn(true)
+//   exports.spawnmanager.forceRespawn()
+// });
+
+on('onClientGameTypeStart', () => {
     emit('chat:addMessage', {
-      args: [`It might have been a good thing that you tried to spawn a ${model}. Who even wants their spawning to actually ^*succeed?`]
-    });
-    return;   
-  }
-
-  // Request the model and wait until the game has loaded it
-  RequestModel(hash);
-  while (!HasModelLoaded(hash))
-  {
-    await Delay(500);
-  }
-
-  const ped = PlayerPedId();
-
-  // Get the coordinates of the player's Ped (their character)
-  const coords = GetEntityCoords(ped);
-
-  // Create a vehicle at the player's position
-  const vehicle = CreateVehicle(hash, coords[0], coords[1], coords[2], GetEntityHeading(ped), true, false);
-
-  // Set the player into the drivers seat of the vehicle
-  SetPedIntoVehicle(ped, vehicle, -1);
-
-  // Allow the game engine to clean up the vehicle and model if needed
-  SetEntityAsNoLongerNeeded(vehicle);
-  SetModelAsNoLongerNeeded(model);
-
-  // Tell the player the car spawned
-  emit('chat:addMessage', {
-    args: [`Woohoo! Enjoy your new ^*${model}!`]
-  });
-}, false);
+        args: [
+            'script bog funciona!'
+        ]
+    })
+});
